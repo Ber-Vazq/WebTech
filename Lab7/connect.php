a886bfaa37199274c28e24481de3a18b91df5d87
Ber-Vazq <bvaq20@hotmail.com>
2023-10-15 21:37:54 -0500

    hopefully this works

--- Lab7/connect.php
+++ Lab7/connect.php
@@ -1,11 +1,4 @@
 <?php
-$con = mysqli_init();
-mysqli_real_connect($con, "wtlabsql.mysql.database.azure.com", getenv("DB_USERNAME"), getenv("DB_PASSWORD"), "wtlabsql", 3306);
-
-if (mysqli_connect_errno()) {
-    die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";
-mysqli_close($con);
+$con=mysqli_init(); 
+mysqli_real_connect($con, "wtlabsql.mysql.database.azure.com", "LabAdmin@wtlabsql", "Enero212002", "wtlabsql", 3306);
 ?>
