ac6b81f7c0148b8e0c3b3e2f6ff99906316ac05b
Ber-Vazq <bvaq20@hotmail.com>
2023-10-19 12:50:23 -0500

    adding the requirements to lab8

--- Lab8/lab8.js
+++ Lab8/lab8.js
@@ -1,4 +1,16 @@
+/*
+Create a function titled getDataFromForm() that:
+Obtains the first name and last name from the form elements in index.html
 
+Calls runAjax and sends the two strings as arguments
+Create a function titled runAjax(fname, lname) that:
+
+Makes an AJAX request to "./ajax.php" using the GET method. The AJAX request should send the
+first and last names as a part of the request.
+
+If the response is a string, change the text of the 
+paragraph element with the id "responseString".
+*/
 function getDataFromForm() {
   alert("it worked!");
 }
