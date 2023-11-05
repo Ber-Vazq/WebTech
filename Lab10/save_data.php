07e84e43cfd3c0df35347103120552890c4586c5
Ber-Vazq <bvaq20@hotmail.com>
2023-11-05 17:23:40 -0600

    PHP NOW

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -1,6 +1,6 @@
 <?php
 //something something opens and reads the data to display on a table in index.html
-if($_SERVER[REQUEST_METHOD] === "POST"){
+if($_SERVER["REQUEST_METHOD"] === "POST"){
     $fsong = $_POST["fsong"];
     $fgenre = $_POST["fgenre"];
     $data = $fsong . " " . $fgenre . "\n";
