d21b63dc8f6512f955698e6ec23966adef07d2f0
Ber-Vazq <bvaq20@hotmail.com>
2023-11-12 07:29:38 -0600

    php stuff

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -1,11 +1,11 @@
 <?php
 //something something opens and reads the data to display on a table in index.html
 if($_SERVER["REQUEST_METHOD"] = "POST"){
-    $fsong = $_POST["fsong"];
-    $fgenre = $_POST["fgenre"];
-    $data = $fsong . " " . $fgenre . "\n";
+    $fname = $_POST["fname"];
+    $lname = $_POST["f"];
+    $data = $fname . " " . $lname . "\n";
 
-    $file = fopen("", "a");
+    $file = fopen("data.txt", "a");
     fwrite($file, $data);
     fclose($file);
 }
