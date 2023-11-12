9245b77b36b8745aab1d42d60015c1341fa62d8f
Ber-Vazq <bvaq20@hotmail.com>
2023-11-12 13:45:53 -0600

    fixing a bug

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -2,7 +2,7 @@
 //something something opens and reads the data to display on a table in index.html
 if($_SERVER["wtLabs"] = "POST"){
     $fname = $_POST["fname"];
-    $lname = $_POST["f"];
+    $lname = $_POST["lname"];
     $data = $fname . " " . $lname . "\n";
 
     $file = fopen("data.txt", "a");
