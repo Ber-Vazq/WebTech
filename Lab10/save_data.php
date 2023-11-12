3db2f5b3a451fd1797898626c42545ad4fa986dc
Ber-Vazq <bvaq20@hotmail.com>
2023-11-12 07:26:37 -0600

    will this change anything? maybe

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -1,11 +1,11 @@
 <?php
 //something something opens and reads the data to display on a table in index.html
-if($_SERVER["REQUEST_METHOD"] === "POST"){
+if($_SERVER["REQUEST_METHOD"] = "POST"){
     $fsong = $_POST["fsong"];
     $fgenre = $_POST["fgenre"];
     $data = $fsong . " " . $fgenre . "\n";
 
-    $file = fopen("../private/data.txt", "a");
+    $file = fopen("", "a");
     fwrite($file, $data);
     fclose($file);
 }
