9f69eeeda11d955be36bed024d5ac80645c4b19a
Ber-Vazq <bvaq20@hotmail.com>
2023-11-12 07:46:28 -0600

    changing

--- Lab10/save_data.php
+++ Lab10/save_data.php
@@ -10,25 +10,3 @@ if($_SERVER["wtLabs"] = "POST"){
     fclose($file);
 }
 ?>
-<!DOCTYPE html>
-<html>
-
-    <head>
-        <title>Lab 10 stored data</title>
-    </head>
-    <body>
-        <h1>Data</h1>
-        <table border="1">
-            <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-            </tr>
-            <?php foreach ($dataArray as $data): ?>
-                <tr>
-                    <td><?php echo $data[0];?></td>
-                    <td><?php echo $data[1];?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    </body>
-</html>
